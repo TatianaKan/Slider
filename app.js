4d70b65d07f60bc4750107fcac88c12dcc4b9d2c
TatianaKan <klanukt@gmail.com>
2021-08-01 13:00:37 +0300

    оптимиз. код под выбор номера активного слайда

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-const slides = document.querySelectorAll('.slide')
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
 
-        slide.classList.add('active')
-    })
+function slidesPlagin(activeSlide = 2) {
+    const slides = document.querySelectorAll('.slide')
+
+    slides[activeSlide].classList.add('active')
+
+    for (const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+
+            slide.classList.add('active')
+        })
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
 }
 
-function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
-}
+slidesPlagin()
